4118dad4aa2e92ca9024ec946ebda1a0ce7ecd6c
Люда <1982luda30@gmail.com>
2022-08-06 01:21:46 +0300

    fetch correct

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,8 @@
 export const fetchCountries = name => {
     const BASE_URL = 'https://restcountries.com/v3.1/name/';
     const properties = 'fields=name,capital,population,flags,languages';
-    return fetch(`${BASE_URL},${name}?${properties}`).then(response => {
+
+    return fetch(`${BASE_URL}${name}?${properties}`).then(response => {
         console.log(response);
         if (!response.ok) {
             throw new Error(response.status);
